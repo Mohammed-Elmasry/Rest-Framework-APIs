684efe753990bb19170cbf9ffe704d171466864c
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-07-21 18:50:10 +0200

    rectify the code in virutalenv

--- snippets/views.py
+++ snippets/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from snippets.models import Snippet
