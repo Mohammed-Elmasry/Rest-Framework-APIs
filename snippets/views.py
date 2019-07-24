6db103795afb05a2ad07af26691471d2a0f3496e
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-07-24 19:24:57 +0200

    rectify the code one more time in the views.py

--- snippets/views.py
+++ snippets/views.py
@@ -96,7 +96,7 @@ class SnippetDetail(APIView):
     def put(self, request, pk, format=None):
         snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet, data=request.data)
-        if serializer.is_valid()
+        if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
