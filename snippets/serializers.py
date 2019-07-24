0765b25b2ffc6a6c8f3466275f10e1da994c6901
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-07-24 23:17:21 +0200

    add owner field to the SnippetSerializer class as ReadOnlyField

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -36,8 +36,8 @@ from django.contrib.auth.models import User
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
-        fields = ["id", "title", "code", "linenos", "language", "style"]
-
+        fields = ["id", "title", "code", "linenos", "language", "style", 'owner']
+        owner = serializers.ReadOnlyField(source='owner.username')
 
 class UserSerializer(serializers.ModelSerializer):
     snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
