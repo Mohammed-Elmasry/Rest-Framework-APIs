cea76a67b57a3b9b1f8da423111076c1863a84fd
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-07-24 20:54:03 +0200

    comment the settings.py portion of the code relevant to the test framework

--- snippets/views.py
+++ snippets/views.py
@@ -1,5 +1,5 @@
 from rest_framework import status
-# from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response # this uses context negotiation to resolve the requested content-type
 from snippets.models import Snippet
