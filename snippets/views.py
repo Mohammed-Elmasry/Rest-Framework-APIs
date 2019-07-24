3716f39b7d4ab32a1d3ad04eb03fa90a56771850
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-07-24 23:18:46 +0200

    add the permissions IsAuthenticatedOrReadOnly to the snippets views (endpoints)

--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,4 @@
+from rest_framework import permissions
 from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.decorators import api_view
@@ -114,6 +115,7 @@ from django.http import Http404
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -124,6 +126,7 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
