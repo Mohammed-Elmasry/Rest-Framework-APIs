8dfc2099e37abe93f56272f54597c30a40bfda5a
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-07-24 22:48:23 +0200

    define the save() method to add html highlighting using pygments

--- snippets/models.py
+++ snippets/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from pygments.lexers import get_all_lexers
+from pygments.lexers import get_all_lexers, get_lexer_by_name
 from pygments.styles import get_all_styles
 from pygments.formatters.html import HtmlFormatter
 from pygments import highlight
@@ -25,3 +25,17 @@ class Snippet(models.Model):
     class Meta:
         ordering = ['created']
 
+
+    # basically like building an on-save method
+
+    def save(self, *args, **kwargs):
+        """
+        use the pygments library to create a highlighted HTML
+        representation of the code snippet
+        """
+        lexer = get_lexer_by_name(self.language)
+        linenos = 'table' if self.linenos else False
+        options = {'title' : self.title} if self.title else {}
+        formatter = HtmlFormatter(style=self.style, linenos=linenos, full=True, **options)
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super(Snippet, self).save(*args, **kwargs)
